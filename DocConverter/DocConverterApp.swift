1c8cc56a65fd33c88904a24fad10c9ae8bef3e8d
pete.j97 <ecjung97@gmail.com>
2026-04-22 10:48:48 +0900

    Initial Commit

---
+++ DocConverter/DocConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocConverterApp.swift
+//  DocConverter
+//
+//  Created by 정은총 on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct DocConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
